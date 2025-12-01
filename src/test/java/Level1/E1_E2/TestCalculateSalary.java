1cd7b055afe9369cac767167bfd12154536e789f
crist <cristhianchulca49@gmail.com>
2025-12-01 23:15:45 +0100

    test: add testCalculateSalaryOld() to test DeprecatedRuntime annotation

--- src/test/java/Level1/E1_E2/TestCalculateSalary.java
+++ src/test/java/Level1/E1_E2/TestCalculateSalary.java
@@ -1,9 +1,10 @@
 package Level1.E1_E2;
 
+import Level1_E1.E2.DeprecatedRuntime;
 import Level1_E1.E2.OnSiteWorker;
 import Level1_E1.E2.OnlineWorker;
 import org.junit.Test;
-
+import java.lang.reflect.Method;
 import static org.junit.Assert.*;
 
 public class TestCalculateSalary {
@@ -16,7 +17,16 @@ public class TestCalculateSalary {
         double salaryOnsiteWorker = onsiteWorker.calculateSalary(100);
         double salaryOnlineWorker = onlineWorker.calculateSalary(160);
 
-        assertEquals(971.50, salaryOnsiteWorker,0.001);
-        assertEquals(1499.16, salaryOnlineWorker,0.001);
+        assertEquals(971.50, salaryOnsiteWorker, 0.001);
+        assertEquals(1499.16, salaryOnlineWorker, 0.001);
+    }
+
+    @Test
+    public void testCalculateSalaryOld() throws NoSuchMethodException {
+        Method deprecatedOnsideWorker = OnSiteWorker.class.getDeclaredMethod("calculateSalaryOld", int.class);
+        Method deprecatedOnlineWorker = OnlineWorker.class.getDeclaredMethod("calculateSalaryOld", int.class);
+
+        assertTrue(deprecatedOnsideWorker.isAnnotationPresent(DeprecatedRuntime.class));
+        assertTrue(deprecatedOnlineWorker.isAnnotationPresent(DeprecatedRuntime.class));
     }
 }
