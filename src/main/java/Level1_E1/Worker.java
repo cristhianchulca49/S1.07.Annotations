788b081169c87d9b3c1a75299799b2cbb5c3b890
crist <cristhianchulca49@gmail.com>
2025-12-01 18:25:11 +0100

    feat: add name, surname and priceHour attributes and calculateSalary() method to calculate the salary

--- src/main/java/Level1_E1/Worker.java
+++ src/main/java/Level1_E1/Worker.java
@@ -1,4 +1,11 @@
 package Level1_E1;
 
 public class Worker {
+    private String name;
+    private String surname;
+    private double priceHour;
+
+    public void calculateSalary(int hoursWorked) {
+        double salary = hoursWorked * priceHour;
+    }
 }
