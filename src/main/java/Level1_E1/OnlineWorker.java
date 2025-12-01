94007ae4e136f53506c4d82971d4f1447fe29e5e
crist <cristhianchulca49@gmail.com>
2025-12-01 19:26:26 +0100

    feat: add @Override implementation of calculateSalary() feat: add INTERNET constant and salary attribute

--- src/main/java/Level1_E1/OnlineWorker.java
+++ src/main/java/Level1_E1/OnlineWorker.java
@@ -1,7 +1,11 @@
 package Level1_E1;
 
 public class OnlineWorker extends Worker {
+    private final double INTERNET = 25.56;
+    private double salary;
+
     @Override
-    public void calculateSalary(int hoursWorked) {
+    public void calculateSalary(int hoursWorkedPerMonth) {
+        salary = (hoursWorkedPerMonth * super.getPriceHour()) + INTERNET;
     }
 }
