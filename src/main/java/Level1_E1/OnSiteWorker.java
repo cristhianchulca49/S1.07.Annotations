169214caa2c2989711a56ddaadeee34d97158c1a
crist <cristhianchulca49@gmail.com>
2025-12-01 20:15:23 +0100

    feat: add constructor to initialize gasoline attribute

--- src/main/java/Level1_E1/OnSiteWorker.java
+++ src/main/java/Level1_E1/OnSiteWorker.java
@@ -3,6 +3,10 @@ package Level1_E1;
 public class OnSiteWorker extends Worker {
     private static double gasoline;
 
+    public OnSiteWorker(double gasolineValue) {
+        gasoline = gasolineValue;
+    }
+
     @Override
     public double calculateSalary(int hoursWorkedPerMonth) {
         return (hoursWorkedPerMonth * super.getPriceHour()) + gasoline;
